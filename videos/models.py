776da48607876ed9c3bd14e9a5b5a52e50e8bc4d
Pric0 <pricaalist@gmail.com>
2020-05-04 19:05:54 +0900

    env added

--- videos/models.py
+++ videos/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Movies(models.Model):
+    title = models.CharField(max_length=129)
+    originaltitle = models.CharField(null=True, blank=True, max_length=129)
+    year = models.DateTimeField(null=True, blank=True)
+    published = models.DateTimeField(auto_now_add=True)
+    plot = models.TextField(null=True, blank=True)
+    poster = models.ImageField(null=True, blank=True, upload_to='cars')
+    landscape = models.ImageField(null=True, blank=True, upload_to='cars')
+    movie_file = models.FileField(null=True, upload_to='uploads/')
--- videos/views.py
+++ videos/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+from .models import Movies
 
 # Create your views here.
+def index(request):
+    movies = Movies.objects
+    return render(request, 'videos\index.html')
